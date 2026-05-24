c18c2d194e63c0361fab8c5e5a156e2a89dfbe9e
Finn <finn.schmidt@campus.lmu.de>
2026-05-24 22:30:40 +0200

    finished YOLOv26Seg Class with train(), test(), predict() and save_model()

--- src/street_sign_project/model.py
+++ src/street_sign_project/model.py
@@ -1,6 +1,7 @@
 # Author: Finn Schmidt
 # Defining wrapper classes for the model for easier application later on
-# Documentation for YOLO under: https://docs.ultralytics.com/reference/engine/model#ultralytics.engine.model.Model.train
+# Documentation on YOLO Website: https://docs.ultralytics.com/reference/engine/model#ultralytics.engine.model.Model.train
+# Detailed Documentation on GitHub: https://github.com/ultralytics/ultralytics/blob/main/ultralytics/engine/model.py    or .../predictior.py
 
 
 from ultralytics import YOLO
@@ -18,7 +19,7 @@ def project_root() -> Path:
     raise FileNotFoundError("pyproject.toml not found")
 
 
-class YOLOv26seg():
+class YOLOv26Seg():
     """Wrapper für YOLO Version 2026 zur Segmentierung"""
     def __init__(self, model_size: str = "s"):
         self.model_size = model_size
@@ -28,7 +29,7 @@ class YOLOv26seg():
         self.model = YOLO(f"yolo26{self.model_size}-seg.pt")
         logger.info(f"Model yolo26{self.model_size}-seg.pt loading completed")
 
-    def train(self, data: str | Path | None = None, model_name: str | None = None, epochs: int = 50, batch_size: int = 16, lr0:float = 0.005, freeze:int=10, device = "cpu"):
+    def train(self, data: str | Path | None = None, model_path: str | None = None, epochs: int = 50, batch_size: int = 16, lr0:float = 0.005, freeze:int=10, device = "cpu"):
         """
         method for fine-tuning the Yolo model
 
@@ -44,9 +45,48 @@ class YOLOv26seg():
         if data is None:
             root = project_root() 
             data = root / "data" / "raw"   #TODO Muss noch angepasst werden und auf die yaml zeigen, wenn sie dann da ist
-        if model_name is None:
-            model_name = f"yolo26{self.model_size}-seg_ep{epochs}_bs{batch_size}_fr{freeze}"
+
         logger.info(f"Start Training yolo26{self.model_size}-seg_ep{epochs}_bs{batch_size}_fr{freeze} at {datetime.now()}")
-        self.model.train(data=str(data), epochs=epochs, freeze=freeze, batch=batch_size, lr0 = lr0, name=model_name, device=device)
+        train_results=self.model.train(data=str(data), epochs=epochs, freeze=freeze, batch=batch_size, lr0 = lr0, device=device)
+        logger.info(f"Training results: {train_results}")
         logger.info(f"Finished training yolo26{self.model_size}-seg_ep{epochs}_bs{batch_size}_fr{freeze} at {datetime.now()}")
+
+        # saving
+        if model_path is not None:
+            self.save_model(model_path)
+            logger.info(f"Model saved under {model_path}")
+
+        return train_results
+
+    def predict(self, new_data: str | Path | None = None, conf:float=0.25):
+        """
+        method for predicting with the model
+
+        params:
+            new_data: file path
+        """
+        if new_data is None or not str(new_data).endswith(".yaml"):
+            raise RuntimeError("Keine Daten für prediction angegeben. Datenverweis muss als .yaml gegeben werden")
+        # Modell wird automatisch in Evaluationsmodus gesetzt 
+        # Data preprocessing für daten im Format (N, 3, H, W) nicht nötig
+        return self.model.predict(source=new_data, conf=conf)
+
+    def test(self, data:str | Path):
+        """
+        Tests Model on test data and returns metrics
+
+        params:
+            data: path to yaml file
+        """
+        if not str(data).endswith(".yaml"):
+            raise RuntimeError("Keine Daten zum Testen angegeben. Datenverweis muss als .yaml gegeben werden")
+        return self.model.val(data=str(data))
+
+    def save_model(self, file_path: str | Path):
+        """
+        method for saving a model under a given path as a .pt file
+        """
+        if not str(file_path).endswith(".pt"):
+            raise ValueError("file path needs to be a \".pt\" file")
+        self.model.save(filename = file_path)
 
